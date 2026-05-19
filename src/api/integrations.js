f79786e684f1251051047880866090d4d825ea50
log5nico <log4nico@gmail.com>
2026-05-19 14:26:02 +0200

    Update integrations.js

--- src/api/integrations.js
+++ src/api/integrations.js
@@ -3,15 +3,15 @@ const db = globalThis.__B44_DB__ || { auth:{ isAuthenticated: async()=>false, me
 
 export const Core = db.integrations.Core;
 
-export const InvokeLLM = db.integrations.Core.InvokeLLM;
+// export const InvokeLLM = db.integrations.Core.InvokeLLM;
 
 export const SendEmail = db.integrations.Core.SendEmail;
 
-export const SendSMS = db.integrations.Core.SendSMS;
+// export const SendSMS = db.integrations.Core.SendSMS;
 
 export const UploadFile = db.integrations.Core.UploadFile;
 
-export const GenerateImage = db.integrations.Core.GenerateImage;
+// export const GenerateImage = db.integrations.Core.GenerateImage;
 
-export const ExtractDataFromUploadedFile = db.integrations.Core.ExtractDataFromUploadedFile;
+// export const ExtractDataFromUploadedFile = db.integrations.Core.ExtractDataFromUploadedFile;
 
